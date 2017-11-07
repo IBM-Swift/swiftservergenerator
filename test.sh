f5ac7a830635cf7f66e4d81c0c30941718bf0136
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-11-07 14:19:40 +0000

    test: improve tests, add gitignore

--- test.sh
+++ test.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
-swiftservergenerator --app --spec '{ "appType": "crud", "appName":"test"}'
+TESTDIR="$TMPDIR/swiftservergenerator-test"
 
-if [ "$?" == "1" ];then
+echo "Cleaning any prior test directory $TESTDIR"
+rm -rf "$TESTDIR"
+
+echo "Creating test directory $TESTDIR"
+mkdir -p "$TESTDIR"
+export PATH=$TESTDIR/bin:$PATH
+
+echo "Installing swiftservergenerator"
+echo "- Creating package"
+npm pack
+export PKG=`ls swiftservergenerator-*.tgz | tail -1`
+echo "- Installing package ($PKG) to test directory as global module"
+npm install -g --prefix="$TESTDIR" "$PKG"
+if [ "$?" != "0" ];then
+  echo "Failed to install"
+  rm -rf "$TESTDIR"
   exit 1
 fi
+echo "Installation complete"
 
-rm -rf swiftserver
+cd "$TESTDIR"
 
+echo "Testing generation of default crud app"
 swiftservergenerator --app --spec '{ "appType": "crud", "appName":"test"}'
-
-if [ "$?" == "1" ];then
+if [ "$?" != "0" ];then
+  echo "Failed"
+  rm -rf "$TESTDIR"
   exit 1
 fi
-
+echo "Cleaning up generated project"
 rm -rf swiftserver
+
+echo "Testing generation of default scaffold app"
+swiftservergenerator --app --spec '{ "appType": "scaffold", "appName":"test"}'
+if [ "$?" != "0" ];then
+  echo "Failed"
+  rm -rf "$TESTDIR"
+  exit 1
+fi
+echo "Cleaning up generated project"
+rm -rf swiftserver
+
+echo "Cleaning up test directory"
+rm -rf $TESTDIR
 
 echo "Testing Suceeded"
